fa8775091b8d8d5b1a0cb1149f2e2d4b0f42a83e
Nikolay Ignatiev <noilgt@gmail.com>
2016-07-15 13:06:09 +0300

    Basics of arrow functions

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,4 +1,5 @@
 // Arrow functions
+// 
 'use strict';
 var invoice = {
     number: 234,
@@ -11,5 +12,14 @@ var newInvoice = {
     number: 123
 };
 
-//Can't bind new object to arrowFunc
+// 1.
+// Can't bind new object to arrowFunc
+// with bind(), call(), apply() can't change value of this
 invoice.process().bind(newInvoice)(); // 234
+
+invoice.process().call(newInvoice);   // 234
+
+// 2.
+// Don't have access to "prototype"
+var getPrice = () => 7.77;
+console.log(getPrice.hasOwnProperty("prototype")); // false
